40a0e7da4f0122c1a5fc5b6f1733a6c6ff53df89
royalmammadov756r <126593256+royalmammadov756r@users.noreply.github.com>
2023-04-27 13:57:04 +0400

    events

---
+++ 5. BOM, Events/app.js
@@ -0,0 +1,49 @@
+// console.log(window.document);
+// console.log(window.screen);
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+
+// btn.addEventListener("click", () => {
+//   //   window.location.assign(
+//   //     "https://www.google.com/search?q=input+type+file&rlz=1C1GCEU_enAZ1036AZ1036&oq=input+type+file&aqs=chrome.0.0i67i650l2j0i512l2j0i20i263i512j0i512l5.5536j0j7&sourceid=chrome&ie=UTF-8"
+//   //   );
+
+//   //   window.history.back();
+//   //   window.history.forward();
+//   window.location.reload();
+// });
+
+// let a = alert('Enter the num')
+
+btn.addEventListener("mouseover", changeColor);
+btn.addEventListener("mouseout", changeGeen);
+
+function changeColor() {
+  btn.style.background = "red";
+}
+
+function changeGeen() {
+  btn.style.background = "green";
+}
+
+input.addEventListener("input", () => {
+  console.log(input.value);
+});
+
+// window.addEventListener("load", () => {
+//   if (document.documentElement.scrollTop > 50) {
+//     body.style.background = "blue";
+//   }
+// });
+
+window.addEventListener("keydown", aa);
+
+function aa(e) {
+  if (e.key == "e") {
+    btn.style.background = "red";
+  } else if (e.key == " ") {
+    btn.style.background = "green";
+  } else {
+    btn.style.background = "pink";
+  }
+}
